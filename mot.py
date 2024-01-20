91a6ba745b8a747b0c4eef5b0e3ebbff3e9b4f07
Chandrahas B <372002chandrahas@gmail.com>
2024-01-20 14:18:51 +0530

    Added functionality to save MOT as an mp4

--- mot.py
+++ mot.py
@@ -4,18 +4,26 @@ from motrackers import CentroidTracker, CentroidKF_Tracker, SORT, IOUTracker
 from motrackers.utils import draw_tracks
 import torch
 
+NEW_WIDTH = 900
+NEW_HEIGHT = 700
 
-def main(video_path, model, tracker):
-    
+def main(video_path, model, tracker, output_video_path):
     cap = cv.VideoCapture(video_path)
-    
+
+    fps = int(cap.get(cv.CAP_PROP_FPS))
+
+    out = cv.VideoWriter(output_video_path,  
+                         cv.VideoWriter_fourcc('m', 'p', '4', 'v'), 
+                         fps, (NEW_WIDTH, NEW_HEIGHT)) 
+
     while True:
         ok, image = cap.read()
+
         if not ok:
             print("Cannot read the video feed.")
             break
 
-        image = cv.resize(image, (900, 700))
+        image = cv.resize(image, (NEW_WIDTH, NEW_HEIGHT))
 
         bboxes, confidences, class_ids = model.detect(image)
         tracks = tracker.update(bboxes, confidences, class_ids)
@@ -23,11 +31,14 @@ def main(video_path, model, tracker):
 
         updated_image = draw_tracks(updated_image, tracks)
 
+        out.write(updated_image)
+
         cv.imshow("image", updated_image)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
+    out.release()
     cv.destroyAllWindows()
 
 
@@ -42,30 +53,11 @@ if __name__ == '__main__':
         '--video', '-v', type=str, default="Sample Videos/Cars waiting for the elderly to cross.mp4", help='Input video path.')
 
     parser.add_argument(
-        '--weights', '-w', type=str,
-        default="models/MOT-YOLOv3/yolov3.weights",
-        help='path to weights file of YOLOv3 (`.weights` file.)'
-    )
-
-    parser.add_argument(
-        '--config', '-c', type=str,
-        default="models/MOT-YOLOv3/yolov3.cfg",
-        help='path to config file of YOLOv3 (`.cfg` file.)'
-    )
-
-    parser.add_argument(
-        '--labels', '-l', type=str,
-        default="models/MOT-YOLOv3/coco_names.json",
-        help='path to labels file of coco dataset (`.names` file.)'
-    )
-
-    parser.add_argument(
-        '--gpu', type=bool,
-        default=True, help='Flag to use gpu to run the deep learning model. Default is `False`'
-    )
-
-    parser.add_argument(
         '--tracker', type=str, default= 'SORT',
+        help="Tracker used to track objects. Options include ['CentroidTracker', 'CentroidKF_Tracker', 'SORT']")
+    
+    parser.add_argument(
+        '--output_path', type=str, default= 'Results/output.mp4',
         help="Tracker used to track objects. Options include ['CentroidTracker', 'CentroidKF_Tracker', 'SORT']")
 
     args = parser.parse_args()
@@ -76,22 +68,19 @@ if __name__ == '__main__':
         tracker = CentroidKF_Tracker(max_lost=0, tracker_output_format='mot_challenge')
     elif args.tracker == 'SORT':
         tracker = SORT(max_lost=3, tracker_output_format='mot_challenge', iou_threshold=0.3)
-    elif args.tracker == 'IOUTracker':
-        tracker = IOUTracker(max_lost=2, iou_threshold=0.5, min_detection_confidence=0.4, max_detection_confidence=0.7,
-                             tracker_output_format='mot_challenge')
     else:
         raise NotImplementedError("Wrong params")
     
 
     model = YOLOv3(
-        weights_path=args.weights,
-        configfile_path=args.config,
-        labels_path=args.labels,
-        confidence_threshold=0.8,
-        nms_threshold=0.2,
-        draw_bboxes=True,
-        use_gpu=args.gpu
+        weights_path= "models/MOT-YOLOv3/yolov3.weights",
+        configfile_path= "models/MOT-YOLOv3/yolov3.cfg",
+        labels_path= "models/MOT-YOLOv3/coco_names.json",
+        confidence_threshold= 0.8,
+        nms_threshold= 0.2,
+        draw_bboxes= True,
+        use_gpu= True
     )
     print('cuda' if torch.cuda.is_available() else 'cpu')
 
-    main(args.video, model, tracker)
+    main(args.video, model, tracker, args.output_path)
